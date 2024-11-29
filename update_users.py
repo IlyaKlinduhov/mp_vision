1de241ab190799e9f9a5264ba6268153b92dbedb
Ilya Klinduhov <ilayklinduhov@gmail.com>
2024-11-29 18:13:51 +0300

    sec_com

--- update_users.py
+++ update_users.py
@@ -20,20 +20,51 @@ async def update_all_users():
 
     categories = get_all_categories(all_users[0]['token'])
 
-    await delete_depricated_rows_in_salesfunel()
-    await delete_depricated_rows_in_adverts()
-    await delete_depricated_rows_in_orders()
-    await delete_depricated_rows_in_sales()
+    try:
+        await delete_depricated_rows_in_salesfunel()
+        await delete_depricated_rows_in_adverts()
+        await delete_depricated_rows_in_orders()
+        await delete_depricated_rows_in_sales()
+    finally:
+        pass
 
     for user in all_users:
         print(f"Начало работы с user {user['user_id']}")
-        await main_update_nmid_sku_for_user(user['token'], user['user_id'], categories)
-        await main_update_advert(user['user_id'], user['token'])
-        await main_salesfunel(user['user_id'], user['token'])
-        await main_sales_update(user['token'], user['user_id'])
-        await main_orders(user['token'], user['user_id'])
+        try:
+            await main_update_nmid_sku_for_user(user['token'], user['user_id'], categories)
+        finally:
+            pass
+
+        try:
+            await main_update_advert(user['user_id'], user['token'])
+        finally:
+            pass
+
+        try:
+            await main_salesfunel(user['user_id'], user['token'])
+        finally:
+            pass
+
+        try:
+            await main_sales_update(user['token'], user['user_id'])
+        finally:
+            pass
+
+        try:
+            await main_orders(user['token'], user['user_id'])
+        finally:
+            pass
+
         print(f"Конец работы с user {user['user_id']}")
 
 
 if __name__ == '__main__':
-    asyncio.run(update_all_users())
+    import asyncio
+
+    async def main():
+        while True:
+            await update_all_users()
+            print("Ожидание перед следующим запуском...")
+            await asyncio.sleep(10)  
+
+    asyncio.run(main())
